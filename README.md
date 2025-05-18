b17d16c7163953c5b65f60d6ae8a3e83d67588d0
Andy <su27t34@gmail.com>
2025-05-18 15:50:12 +0800

    增加註冊頁面,完成登入/註冊api串接,警示訊息&版面互動微調

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# food_map_project
+# 車站美食地圖 前端前台
+現在很多年輕人會做無計畫的一日火車旅行，所以我們想做一個火車美食地圖讓人們可以在快速地找到餐廳減少不必要的時間消耗。
+
+## 技術棧
+語言：HTML, vanilla javascript
+樣式：vanilla CSS
+API 串接：RESTful API（與後端協作）
+版本控制：Git + GitHub
--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // 初始化地圖，中心點設在台北車站，縮放等級 17
 const map = L.map('map').setView([25.0478, 121.5170], 17);
 
+// const清單
+const baseUrl = new URL('https://north11.onrender.com/api/users');
+// const baseUrl = new URL('http://localhost:8000/api/users/');
+let token = "";
+
 // 加入 OpenStreetMap 圖層
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '地圖資料 © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> 貢獻者'
@@ -31,21 +36,106 @@ function showLogin() {
 
 function hideLogin() {
   document.getElementById('overlay').classList.remove('show');
+  document.getElementById("login-box").style.display = "block"; //恢復原狀
+  document.getElementById("signup-box").style.display = "none";
 }
 
-
+// 登入函數
 function login() {
-  const user_name = document.getElementById('test-username');
+  const user_name = document.getElementById('login-username');
+  const password = document.getElementById('login-password')
   const button = document.getElementById('user-btn')
-  button.textContent = user_name.value;
-  document.getElementById('overlay').classList.remove('show');
-  document.getElementById("user-box-default").style.display = "none";
-  document.getElementById("user-box-login").style.display = "block";
+  const errorTxt = document.getElementById('error-text-login')
+  // 呼叫登入api
+  const UrlLogin = new URL('log-in', baseUrl);
+  fetch(UrlLogin.href, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      // "email": "a1234567@gmail.com",
+      // "password": "Aa123456"
+      "email": user_name.value,
+      "password": password.value
+    })
+  })
+    .then(response => response.json())
+    .then(data=>{
+      console.log(data);
+      if (data.status === 'success') {
+        button.textContent = data['data']['user']['name'];
+        token = data['data']['token'];
+        document.getElementById('overlay').classList.remove('show');
+        document.getElementById("user-box-default").style.display = "none";
+        document.getElementById("user-box-login").style.display = "block";
+      }
+      else if (data.status != 'success') {
+        // 這裡手動拋出錯誤，把錯誤訊息一起拋出
+        throw new Error(data.message || '登入失敗');
+      }
+    })
+    .catch(error => {
+      console.error('錯誤:', error);
+      errorTxt.textContent = error['message'];
+      setTimeout(function(){
+        errorTxt.textContent = '';
+      }, 3000)
+    });
 }
 
 function logOut() {
   document.getElementById("user-box-default").style.display = "block";
   document.getElementById("user-box-login").style.display = "none";
+}
+
+// 登入頁面轉註冊頁面
+function toSignIn() {
+  document.getElementById("login-box").style.display = "none";
+  document.getElementById("signup-box").style.display = "flex";
+}
+
+// 註冊函數
+function signUp() {
+  const userName = document.getElementById('signup-username');
+  const email = document.getElementById('signup-email');
+  const password = document.getElementById('signup-password');
+  const errorTxt = document.getElementById('error-text-signup');
+  // 呼註冊api
+  const UrlSignUp = new URL('sign-up', baseUrl);
+  console.log(UrlSignUp.href)
+  fetch(UrlSignUp.href, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "name": userName.value,
+      "email": email.value,
+      "password": password.value
+    })
+  })
+    .then(response => response.json())
+    .then(data=>{
+      // console.log(data);
+      if (data.status === 'success') {
+        document.getElementById('overlay').classList.remove('show');
+        document.getElementById("login-box").style.display = "block"; //恢復原狀
+        document.getElementById("signup-box").style.display = "none";
+      }
+      else if (data.status != 'success') {
+        throw new Error(data.message || '登入失敗');
+      }
+    })
+    .catch(error => {
+      console.error('錯誤:', error);
+      errorTxt.textContent = error['message'];
+      setTimeout(function(){
+        errorTxt.textContent = "";
+      }, 3000)
+    });
+
+  
 }
 
 // 已登陸頁面: 新增使用者選單開關功能
